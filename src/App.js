91f57fb495d130a702f2aba8d32dad32cc81ddb9
Os-Traveller <faisal.ahmed.20.35.197@gmail.com>
2022-03-21 21:16:31 +0600

    Data removed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import Header from "./Components/header/Header";
 
 function App() {
   // * User data
-  const allusers = {
-    users: {
-      faisal: {
-        email: "faisal@gmail.com",
-        password: "fuck_you",
-        cart: ["product_1"],
-      },
-    },
-  };
   return (
     <div className="App">
       <Header />
